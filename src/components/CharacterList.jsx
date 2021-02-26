a234b953d5859bcaa4d4e88dce41f59db15ef189
Katherine <katherine.briceno@zooplus.com>
2021-02-26 18:18:04 +0100

    Added uuid package and painted the list

--- src/components/CharacterList.jsx
+++ src/components/CharacterList.jsx
@@ -1,16 +1,26 @@
 
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 
 function CharacterList() {
+
+  const [characters, setCharacters] = useState([]);
 
   useEffect(() => {
     fetch('https://simpsons-quotes-api.herokuapp.com/quotes?count=10')
     .then(response => response.json())
-    .then(data => console.log(data));
+    .then(data => setCharacters(data));
   }, []);
 
   return (
-    <div>LISTA</div>
+    <div>{characters.map(character => {
+      return (
+      <li key={uuidv4()}>
+        <h3>{character.character}</h3>
+        <img src={character.image} alt={character.character}/>
+        <p>{character.quote}</p>
+      </li>)
+    })}</div>
   )
 }
 
